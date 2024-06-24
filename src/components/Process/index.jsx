4286411cf84be8dc604d8e384dc4cdfe32182718
NurjahonErgashevMe <140044620+NurjahonErgashevMe@users.noreply.github.com>
2024-06-24 21:06:19 +0500

    changed header on contacts and changed grid images on services

--- src/components/Process/index.jsx
+++ src/components/Process/index.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import PhotoGrid from './PhotoGrid';
-import ProcessItem from './ProcessItem';
-import processItemData from './processItemData';
+import React from "react";
+import PhotoGrid from "./PhotoGrid";
+import ProcessItem from "./ProcessItem";
+import processItemData from "./processItemData";
+
+import ShipImage1 from "../../assets/img/ship/ship1.jpg";
+import ShipImage2 from "../../assets/img/ship/ship2.jpg";
+import ShipImage3 from "../../assets/img/ship/sea_transportation.jpg";
 
 function Process() {
-    return (
-        <section className="process-wrapper section-padding section-bg">
-            <div className="container">
-                <div className="row align-center">
-                    <PhotoGrid />
-                    <div className="col-xl-6 col-12 offset-xl-1">
-                        <div className="section-title">
-                            <span>process</span>
-                            <p>process</p>
-                            <h1>Our Company Work Process.</h1>
-                        </div>
-
-                        <div className="process-setps mt-5 mt-xl-0">
-                            {processItemData.map((data) => (
-                                <ProcessItem
-                                    key={data.key}
-                                    number={data.number}
-                                    heading={data.heading}
-                                    text={data.text}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="process-wrapper section-padding section-bg">
+      <div className="container">
+        <div className="row align-center">
+          <PhotoGrid img1={ShipImage1} img2={ShipImage2} img3={ShipImage3} />
+          <div className="col-xl-6 col-12 offset-xl-1">
+            <div className="section-title">
+              <span>process</span>
+              <p>process</p>
+              <h1>Our Company Work Process.</h1>
             </div>
-        </section>
-    );
+
+            <div className="process-setps mt-5 mt-xl-0">
+              {processItemData.map((data) => (
+                <ProcessItem
+                  key={data.key}
+                  number={data.number}
+                  heading={data.heading}
+                  text={data.text}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Process;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import bannerBg from '../assets/img/page-banner.jpg';
 import ContactForm from '../components/ContactForm';
 import ContactUs from '../components/ContactUs';
-import Footer1 from '../components/Footer1';
-import Header1 from '../components/Header1';
+import Footer from '../components/Footer3';
+import Header from '../components/Header1';
 import PageBanner from '../components/PageBanner';
 
 function Contact() {
     return (
         <>
-            <Header1 />
+            <Header />
             <PageBanner bannerBg={bannerBg} heading="Contact Us" currentPage="Contact" />
             <ContactUs />
             <ContactForm title="Write Message" heading="Get In Touch" />
-            <Footer1 />
+            <Footer />
         </>
     );
 }
