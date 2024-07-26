2e9945d853a527e18099bac7fa8907c7f8ab3ae7
NurjahonErgashevMe <140044620+NurjahonErgashevMe@users.noreply.github.com>
2024-07-26 14:55:09 +0500

    updated phone number

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const PHONE_NUMBER = "(888) 996-4433"
+export const PHONE_NUMBER = "(513) 734-8757"
 export const EMAIL = "info@refineship.com"
 export const ADDRESS = `"5718 Richmond Park Drive, Mason, OH, 45040, USA "`
 export const ADDRESS_LINK = "https://maps.app.goo.gl/EpMhpuP7ZBPhpA5a6"
